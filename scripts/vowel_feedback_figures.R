31ef33cc488e1a5ed5c652ab4d742c459e150380
Page Piccinini <page.piccinini@gmail.com>
2016-12-03 07:38:34 +0100

    Add commenting. Add function for making base plots.

--- scripts/vowel_feedback_figures.R
+++ scripts/vowel_feedback_figures.R
@@ -1,5 +1,7 @@
 ## READ IN DATA ####
+# Prototypical vowels for plot
 data_proto = read.table("data/data_prototypes.txt", header=T, sep="\t") %>%
+  # Reset IPA coding
   mutate(vowel_ipa = if_else(vowel == "E", "ɛ",
                      if_else(vowel == "I", "ɪ",
                      if_else(vowel == "ae", "æ",
@@ -9,114 +11,36 @@ data_proto = read.table("data/data_prototypes.txt", header=T, sep="\t") %>%
                      if_else(vowel == "c", "ɔ",
                      if_else(vowel == "er", "ɝ",as.character(vowel))))))))))
 
+# Recording specific formant information
 data_formants_clean = data_formants %>%
-  #filter(time > 0.1) %>%
-  #filter(time < 0.4) %>%
+  # Add IPA column
   mutate(vowel_ipa = vowel) %>%
+  # Bin data by 0.5s intervals
   mutate(bin = floor(time / 0.5)) %>%
+  # Get median f1 and f2 by bins
   group_by(vowel_ipa, bin) %>%
   summarise(f1_median  = median(f1, na.rm = T),
             f2_median = median(f2, na.rm = T)) %>%
   ungroup()
 
 
-## MAKE BASE FIGURE ####
+## MAKE BASE FIGURES ####
 # Make base plot for Hagiwara (1997), female speaker
-base_hagi_f.plot = data_proto %>%
-  # Set study
-  filter(study == "Hagiwara (1997)") %>%
-  # Set gender
-  filter(gender == "female") %>%
-  # Make plot
-  ggplot(aes(x = f2, y = f1, label = vowel_ipa)) +
-  geom_point(size = 4) +
-  geom_text(nudge_x = 60, nudge_y = -20, size = 6) +
-  geom_segment(aes(x = max(f2) + 150, xend = max(f2) - ((max(f2) - min(f2)) / 2),
-                   y = min(f1) - 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = max(f2) - ((max(f2) - min(f2)) / 2), xend = min(f2 - 100),
-                   y = max(f1) + 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = min(f2) - 100,
-                   y = max(f1) + 50, yend = min(f1) - 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = max(f2) + 150,
-                   y = min(f1) - 50, yend = min(f1) - 50)) +
-  scale_x_reverse() +
-  scale_y_reverse() +
-  theme_void() +
-  theme(text = element_text(size = 16))
+base_hagi_f.plot = base_plot(data_proto, "Hagiwara (1997)", "female")
 
 # Make base plot for Hagiwara (1997), male speaker
-base_hagi_m.plot = data_proto %>%
-  # Set study
-  filter(study == "Hagiwara (1997)") %>%
-  # Set gender
-  filter(gender == "male") %>%
-  # Make plot
-  ggplot(aes(x = f2, y = f1, label = vowel_ipa)) +
-  geom_point(size = 4) +
-  geom_text(nudge_x = 60, nudge_y = -20, size = 6) +
-  geom_segment(aes(x = max(f2) + 150, xend = max(f2) - ((max(f2) - min(f2)) / 2),
-                   y = min(f1) - 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = max(f2) - ((max(f2) - min(f2)) / 2), xend = min(f2 - 100),
-                   y = max(f1) + 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = min(f2) - 100,
-                   y = max(f1) + 50, yend = min(f1) - 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = max(f2) + 150,
-                   y = min(f1) - 50, yend = min(f1) - 50)) +
-  scale_x_reverse() +
-  scale_y_reverse() +
-  theme_void() +
-  theme(text = element_text(size = 16))
+base_hagi_m.plot = base_plot(data_proto, "Hagiwara (1997)", "male")
 
 # Make base plot for Hillenbrand et al. (1995), female speaker
-base_hill_f.plot = data_proto %>%
-  # Set study
-  filter(study == "Hillenbrand et al. (1995)") %>%
-  # Set gender
-  filter(gender == "female") %>%
-  # Make plot
-  ggplot(aes(x = f2, y = f1, label = vowel_ipa)) +
-  geom_point(size = 4) +
-  geom_text(nudge_x = 60, nudge_y = -20, size = 6) +
-  geom_segment(aes(x = max(f2) + 150, xend = max(f2) - ((max(f2) - min(f2)) / 2),
-                   y = min(f1) - 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = max(f2) - ((max(f2) - min(f2)) / 2), xend = min(f2 - 100),
-                   y = max(f1) + 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = min(f2) - 100,
-                   y = max(f1) + 50, yend = min(f1) - 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = max(f2) + 150,
-                   y = min(f1) - 50, yend = min(f1) - 50)) +
-  scale_x_reverse() +
-  scale_y_reverse() +
-  theme_void() +
-  theme(text = element_text(size = 16))
+base_hill_f.plot = base_plot(data_proto, "Hillenbrand et al. (1995)", "female")
 
 # Make base plot for Hillenbrand et al. (1995), male speaker
-base_hill_m.plot = data_proto %>%
-  # Set study
-  filter(study == "Hillenbrand et al. (1995)") %>%
-  # Set gender
-  filter(gender == "male") %>%
-  # Make plot
-  ggplot(aes(x = f2, y = f1, label = vowel_ipa)) +
-  geom_point(size = 4) +
-  geom_text(nudge_x = 60, nudge_y = -20, size = 6) +
-  geom_segment(aes(x = max(f2) + 150, xend = max(f2) - ((max(f2) - min(f2)) / 2),
-                   y = min(f1) - 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = max(f2) - ((max(f2) - min(f2)) / 2), xend = min(f2 - 100),
-                   y = max(f1) + 50, yend = max(f1) + 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = min(f2) - 100,
-                   y = max(f1) + 50, yend = min(f1) - 50)) +
-  geom_segment(aes(x = min(f2) - 100, xend = max(f2) + 150,
-                   y = min(f1) - 50, yend = min(f1) - 50)) +
-  scale_x_reverse() +
-  scale_y_reverse() +
-  theme_void() +
-  theme(text = element_text(size = 16))
+base_hill_m.plot = base_plot(data_proto, "Hillenbrand et al. (1995)", "male")
 
 
 ## ADD SPEAKER SPECIFIC POINTS TO A BASE ####
-base_hagi_m.plot +
-#base_hill_m.plot +
+#base_hagi_m.plot +
+base_hill_m.plot +
   geom_point(data = data_formants_clean, aes(x = f2_median, y = f1_median),
              color = "red", size = 3) +
   geom_path(data = data_formants_clean, aes(x = f2_median, y = f1_median),
--- scripts/vowel_feedback_functions.R
+++ scripts/vowel_feedback_functions.R
@@ -1,3 +1,4 @@
+## ACOUSTIC ANALYSIS ####
 # Get formant information from a sound file
 formants_extracter = function(vowelfile, formant_arguments) {
 
@@ -6,6 +7,7 @@ formants_extracter = function(vowelfile, formant_arguments) {
   formant_loc = sub(wav_loc, pattern = ".wav", replacement = ".Formant")
   table_loc = sub(wav_loc, pattern = ".wav", replacement = ".txt")
   
+  # Set arguments for tables
   tab_args = list( TRUE, # Include frame number
                    TRUE, # Include time
                    3,    # Time decimals
@@ -15,26 +17,66 @@ formants_extracter = function(vowelfile, formant_arguments) {
                    3,    # Frequency decimals
                    TRUE )# Include bandwidths
   
+  # Perform Praat call to get formants based on input wav
   praat("To Formant (burg)...",
         arguments = formant_arguments,
         input = wav_loc,
         output = formant_loc)
   
+  # Perform Praat call to save formant information to a table
   praat("Down to Table...",
         arguments = tab_args,
         input = formant_loc,
         output = table_loc,
         filetype = "tab-separated")
   
+  # Read in table and clean up data
   read.table(file = table_loc, header=TRUE, sep="\t", na.strings="--undefined--" ) %>%
+    # Only keep time, F1, and F2
     select(time.s., F1.Hz., F2.Hz.) %>%
+    # Rename time, f1, and f2 columns
     rename(time = time.s.) %>%
     rename(f1 = F1.Hz.) %>%
     rename(f2 = F2.Hz.) %>%
+    # Save vowel and gender information for intial settings
     mutate(vowel = vowel) %>%
     mutate(gender = gender)
-#    gather(formant, value, F1.Hz.:B5.Hz.) %>%
-#    filter(formant == "F1.Hz." | formant == "F2.Hz." | formant == "F3.Hz.") %>%
- #   mutate(formant = factor(formant, levels = c("F1.Hz.", "F2.Hz.", "F3.Hz."),
-                           # labels = c("f1", "f2", "f3")))
+  
+  #unlink(formant_loc)
+  #unlink(table_loc)
+
+}
+
+
+## FIGURE MAKING ####
+# Make base plots
+base_plot = function(data, specific_study, specific_gender) {
+  data %>%
+    # Set study
+    filter(study == specific_study) %>%
+    # Set gender
+    filter(gender == specific_gender) %>%
+    # Make plot with f2 as x, f1 and y, and IPA vowel coding as labels
+    ggplot(aes(x = f2, y = f1, label = vowel_ipa)) +
+    # Add vowel points
+    geom_point(size = 4) +
+    # Move over labels
+    geom_text(nudge_x = 60, nudge_y = -20, size = 6) +
+    # Make lines for trapizoid
+    geom_segment(aes(x = max(f2) + 150, xend = max(f2) - ((max(f2) - min(f2)) / 2),
+                     y = min(f1) - 50, yend = max(f1) + 50)) +
+    geom_segment(aes(x = max(f2) - ((max(f2) - min(f2)) / 2), xend = min(f2 - 100),
+                     y = max(f1) + 50, yend = max(f1) + 50)) +
+    geom_segment(aes(x = min(f2) - 100, xend = min(f2) - 100,
+                     y = max(f1) + 50, yend = min(f1) - 50)) +
+    geom_segment(aes(x = min(f2) - 100, xend = max(f2) + 150,
+                     y = min(f1) - 50, yend = min(f1) - 50)) +
+    # Reverse x and y axes so more like a vowel chart
+    scale_x_reverse() +
+    scale_y_reverse() +
+    # Remove all other axes
+    theme_void() +
+    # Increase font size
+    theme(text = element_text(size = 16))
+  
 }
